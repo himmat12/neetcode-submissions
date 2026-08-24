e5333c354fcda1aec301857049f362db01578b3d
Himmat Rai <48753714+himmat12@users.noreply.github.com>
2026-08-24 16:31:35 +0100

    Add: is-palindrome - submission-9

---
+++ Data Structures & Algorithms/is-palindrome/submission-9.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        arr = []
+        for c in s:
+            if c.isalnum():
+                arr.append(c.lower())
+        l = 0
+        r = len(arr) - 1
+        print(arr)
+        while l <= r:
+            print(l, r)
+            if arr[l] != arr[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
